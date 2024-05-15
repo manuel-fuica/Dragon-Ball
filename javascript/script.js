3af39c4c6c4042e6d9134c57639564e4b271124e
Manuel Fuica <manuelfuica13@gmail.com>
2024-05-15 19:26:34 -0400

    incorporacion de flecha hacia inicio

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,27 @@
 
 
-let menuToggle = document.getElementById("menu-toggle");
-let navbar = document.getElementById("navbar");
+// let menuToggle = document.getElementById("menu-toggle");
+// let navbar = document.getElementById("navbar");
 
-function toggleMenu(){
-    navbar.style.display = (navbar.style.display === 'none' || navbar.style.display === '') ? 'block' : 'none';
+// function toggleMenu(){
+//     navbar.style.display = (navbar.style.display === 'none' || navbar.style.display === '') ? 'block' : 'none';
+// }
+
+// menuToggle.addEventListener('click', toggleMenu);
+
+// Mostrar el botón al hacer scroll
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+    } else {
+    document.getElementById("myBtn").style.display = "none";
+    }
 }
 
-menuToggle.addEventListener('click', toggleMenu);
+// Subir al principio de la página
+function topFunction() {
+  document.body.scrollTop = 0; // Para Safari
+  document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE y Opera
+}
